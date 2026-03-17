a30346c0f779028e57f77ff6576146ccc41d28b4
Michel  Viana <michelrviana@gmail.com>
2026-03-17 11:20:16 -0300

    Teste.py inserido outra função

--- teste.py
+++ teste.py
@@ -2,3 +2,10 @@ def login_user(username, password):
     if password == "admin123":
         return True
     return False
+
+
+def login_user_new(username, password):
+    if password == "123admin123":
+        return True
+    return False
+
